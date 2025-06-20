7a4532a9bf593a562eb9b9b3f6c03c475addccbc
sentosamemet <sentosamemet@gmail.com>
2025-06-20 14:05:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,8 +30,7 @@ echo "ChromeDriver installed."
 
 # Install Google Chrome
 echo "Installing Google Chrome..."
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
-dpkg -i google-chrome-stable_current_amd64.deb && \
+apt install chromium-browser -y && \
 apt-get install -f -y && \
 apt --fix-broken install -y && \
 echo "Google Chrome installed."
