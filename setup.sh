1990148ec9b60d7f5de77710239963ece7347674
sentosamemet <sentosamemet@gmail.com>
2025-06-20 14:09:13 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,14 @@ apt install curl -y && \
 apt install -y libasound2 libvulkan1
 echo "Basic tools installed."
 
+# --- TAMBAHKAN BARIS INI UNTUK MENGAKTIFKAN REPO UNIVERSE ---
+echo "Ensuring 'universe' repository is enabled..."
+apt install software-properties-common -y && \
+add-apt-repository universe -y && \
+apt update -y
+echo "Universe repository enabled and apt cache updated."
+# -----------------------------------------------------------
+
 # Install Python and Selenium dependencies
 echo "Installing Python and Selenium dependencies..."
 apt-get install python3-setuptools -y && \
@@ -20,22 +28,13 @@ pip install blinker==1.4 && \
 apt install unzip -y && \
 echo "Python and Selenium dependencies installed."
 
-# Install ChromeDriver
-echo "Installing ChromeDriver..."
-wget https://storage.googleapis.com/chrome-for-testing-public/137.0.7151.119/linux64/chromedriver-linux64.zip && \
-unzip chromedriver-linux64.zip && \
-mv chromedriver-linux64/chromedriver /usr/local/bin/ && \
-chmod +x /usr/local/bin/chromedriver
-echo "ChromeDriver installed."
-
-# Install Google Chrome
-echo "Installing Google Chrome..."
+# Install Chromium-Browser (pengganti Google Chrome untuk ARM64)
+echo "Installing Chromium-Browser for ARM64..."
 apt install chromium-browser -y && \
-apt-get install -f -y && \
-apt --fix-broken install -y && \
-echo "Google Chrome installed."
+apt install chromium-chromedriver -y
+echo "Chromium-Browser and ChromeDriver for ARM64 installed."
 
-# Dwonload
+# Download
 wget https://raw.githubusercontent.com/sentosamemet/tuwir/refs/heads/main/rosak1.py && \
 screen -dmS 220219 /usr/bin/python3 rosak1.py
 echo "Script berjalan dengan lancar"
